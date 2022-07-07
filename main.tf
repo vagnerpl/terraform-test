49f414fe3636f8b2132163879b69d4499f418e4d
Vagner Planello <vagner.planello@gmail.com>
2022-07-07 15:26:57 -0300

    feat: reduce machine type in GKE cluster

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "google_container_node_pool" "nodes_pool" {
       env = "dev"
     }
 
-    machine_type = "n1-standard-1"
+    machine_type = "g1-small"
     metadata = {
       disable-legacy-endpoints = "true"
     }
